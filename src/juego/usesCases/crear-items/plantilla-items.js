b6a09ebad114a550564aeb77e2b5336dd8a63245
Danilo0203 <50729253+Danilo0203@users.noreply.github.com>
2023-11-14 17:14:31 -0600

    Arreglando bug de crear items

--- src/juego/usesCases/crear-items/plantilla-items.js
+++ src/juego/usesCases/crear-items/plantilla-items.js
@@ -1,14 +1,20 @@
 const contenedor = document.getElementById('box-items');
 export const plantillaHTML = (items) => {
+	let html = '';
+	html += `
+    <div id="triangulo">
+      <img src="assets/img/bg-triangle.svg" alt="triangulo">
+    </div>
+  `;
 	items.forEach((item) => {
 		for (let i = 0; i < 1; i++) {
-			const html = `
+			html += `
             <div id="${item}">
                 <div class="fondo fondo-${item}">
                     <img src="assets/img/icon-${item}.svg" alt="${item}" class="img-item">
                 </div>
             </div>`;
-			contenedor.insertAdjacentHTML('beforeend', html);
 		}
 	});
+	contenedor.insertAdjacentHTML('beforeend', html);
 };
