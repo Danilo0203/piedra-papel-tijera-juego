46833b4ce54ac63d063422ea59c0691c5722e05b
Danilo0203 <50729253+Danilo0203@users.noreply.github.com>
2023-11-14 14:11:24 -0600

    Arreglar estilo responsivo

--- src/juego/usesCases/Listeners/btn-reglas.js
+++ src/juego/usesCases/Listeners/btn-reglas.js
@@ -1,12 +1,15 @@
 const btnReglas = document.getElementById('btn-reglas');
+const contenedorReglas = document.querySelector('.reglas');
 const btnCerrar = document.querySelector('.btn-cerrar');
 const modalRules = document.getElementById('rules');
 const juego = document.querySelector('.juego');
 btnReglas.addEventListener('click', () => {
 	modalRules.classList.remove('hidden');
+	contenedorReglas.classList.add('hidden');
 	juego.classList.add('opacidad');
 });
 btnCerrar.addEventListener('click', () => {
 	modalRules.classList.add('hidden');
 	juego.classList.remove('opacidad');
+	contenedorReglas.classList.remove('hidden');
 });
