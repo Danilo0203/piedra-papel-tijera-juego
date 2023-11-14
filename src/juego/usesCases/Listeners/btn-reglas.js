24ae761a9823a1f539e7a1700a1f736d50702824
Danilo0203 <50729253+Danilo0203@users.noreply.github.com>
2023-11-14 14:55:43 -0600

    Arreglar estilos responsivos creditos

--- src/juego/usesCases/Listeners/btn-reglas.js
+++ src/juego/usesCases/Listeners/btn-reglas.js
@@ -3,13 +3,16 @@ const contenedorReglas = document.querySelector('.reglas');
 const btnCerrar = document.querySelector('.btn-cerrar');
 const modalRules = document.getElementById('rules');
 const juego = document.querySelector('.juego');
+const jugarOtravez = document.querySelector('.ganador');
 btnReglas.addEventListener('click', () => {
 	modalRules.classList.remove('hidden');
 	contenedorReglas.classList.add('hidden');
 	juego.classList.add('opacidad');
+	jugarOtravez.classList.add('hidden');
 });
 btnCerrar.addEventListener('click', () => {
 	modalRules.classList.add('hidden');
 	juego.classList.remove('opacidad');
 	contenedorReglas.classList.remove('hidden');
+	jugarOtravez.classList.remove('hidden');
 });
