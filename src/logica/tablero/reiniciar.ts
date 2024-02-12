469457ae10411bd6f47bad388efc2a344f0b02c6
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-02-12 11:16:34 -0600

    build game

--- src/logica/tablero/reiniciar.ts
+++ src/logica/tablero/reiniciar.ts
@@ -10,7 +10,6 @@ export const reiniciar = () => {
   const jugadores: string[] = ["jugador", "casa"];
   const tablero = elemtId("tablero-juego");
   let itemCasa = itemAleatorio(items);
-  console.log(`reiniciar: ${itemCasa}`);
   tableroRender(tablero);
   clickItems(items, (itemClick: string) => {
     setTimeout(() => {
